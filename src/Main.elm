8cbdbd45b67a2640650191db208c16e672cffa53
SamAxe <693896-SamAxe@users.noreply.gitlab.com>
2024-10-05 17:31:19 -0700

    Working towards editting a paragraph

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,27 @@
 module Main exposing (..)
 
 import Browser
--- import Html exposing (..)
--- import Html.Attributes exposing (style)
+import Browser.Navigation as Nav
+import Element.Background as Background
+import Element.Border as Border
+import Element exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+import Element.Events as Events
+-- import Html.Attributes exposing (..)
 -- import Html.Events exposing (..)
+-- import Html exposing (..)
 import Http
 import Json.Decode
-
-import Browser.Navigation as Nav
+import MarkdownUi
+import Parser exposing (Parser, (|.), (|=) )
 import Url
 import Url.Parser exposing ( (</>) )
 -- import Url.Route
-
-
-import Element exposing (..)
-import Element.Background as Background
-import Element.Border as Border
-import Element.Font as Font
-
-import WikiHelper
 import Wiki
-import MarkdownUi
 import Element.Input as Input
-
+import List exposing (tail)
+import Internal.Model exposing (Description(..))
 
 -- MAIN
 
@@ -44,15 +43,17 @@ main =
 
 
 type alias Model =
-  { key : Nav.Key
-  , url : Url.Url
-  , stories : List Story
+  { key       : Nav.Key
+  , url       : Url.Url
+  , stories   : List Story
+  , ptext     : Maybe String
+  , pselected : Maybe ParaId
   }
 
 
 type alias Paragraph =
   { type_ : String
-  , id    : String
+  , id    : Maybe ParaId
   , text  : Maybe String
   }
 
@@ -61,11 +62,12 @@ type alias Story =
   , story  : List Paragraph
   }
 
+type ParaId = ParaId String
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
 --  ( Model key url { title = "blank", story = [] }, Cmd.none )
-  ( Model key url [ ], getRandomStory (Wiki.asSlug "Welcome Visitors"))
+  ( Model key url [ ] Nothing Nothing, getRandomStory (Wiki.asSlug "Welcome Visitors"))
 
 
 --  (Loading, getRandomStory)
@@ -80,6 +82,9 @@ type Msg
   = LinkClicked Browser.UrlRequest
   | UrlChanged Url.Url
   | GotStory (Result Http.Error Story)
+  | TextChanged String
+  | SelectParagraph (Maybe ParaId)
+  | SaveEditItem
 
 
 -- type Route =
@@ -128,52 +133,61 @@ update msg model =
 
         Err _ -> ( model, Cmd.none)
 
+    TextChanged pt ->
+        ( { model | ptext = Just pt}, Cmd.none)
+
+    SelectParagraph para_id ->
+      ( { model | pselected = para_id, ptext = getSelection para_id model.stories}, Cmd.none)
+
+    SaveEditItem -> 
+      ( { model | stories = updateSelection model.pselected model.ptext model.stories
+        , ptext = Nothing
+        , pselected = Nothing
+        }
+      , Cmd.none )
+
+
+getSelection : Maybe ParaId -> List Story -> Maybe String
+getSelection paraId stories =
+  case stories of
+    [] -> Nothing
+    hd::tail ->
+      case getParagraphSelection paraId hd.story of
+          Nothing -> getSelection paraId tail
+          Just p  -> Just p
+
+getParagraphSelection : Maybe ParaId -> List Paragraph -> Maybe String
+getParagraphSelection paraId paragraphs =
+  case paragraphs of
+    [] -> Nothing
+    hd::tail ->
+      if hd.id == paraId
+      then hd.text
+      else getParagraphSelection paraId tail
+
+
+updateSelection : Maybe ParaId -> Maybe String -> List Story -> List Story
+updateSelection paraId new_text stories =
+  List.map (updateStory paraId new_text) stories
+
+updateStory : Maybe ParaId -> Maybe String -> Story -> Story
+updateStory paraId new_text story =
+  { story | story = List.map (updateParagraph paraId new_text) story.story }
+
+
+updateParagraph : Maybe ParaId -> Maybe String -> Paragraph -> Paragraph
+updateParagraph paraId new_text paragraph =
+  if paraId == paragraph.id
+  then { paragraph | text = new_text }
+  else paragraph
+
+
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
   Sub.none
-
-
-
--- VIEW
-
-
--- view : Model -> Browser.Document Msg
--- view model =
---   { title = "URL Interceptor"
---   , body =
---       [ text "The current URL is: "
---       , b [] [ text (Url.toString model.url) ]
---       , ul []
---           [ viewLink "/home"
---           , viewLink "/profile"
---           , viewLink "/reviews/the-century-of-the-self"
---           , viewLink "/reviews/public-opinion"
---           , viewLink "/reviews/shah-of-shahs"
---           ]
---       ]
---   }
--- 
--- 
--- viewLink : String -> Html msg
--- viewLink path =
---   li [] [ a [ href path ] [ text path ] ]
--- 
-
-
--- view : Model -> Browser.Document Msg
--- view model =
---   { title = "URL Interceptor"
---   , body = [ div []
---     [ text "The current URL is: "
---     , b [] [ text (Url.toString model.url) ]
---     , h2 [] [ text "Random Story" ]
---     , viewStories model
---     ]
---     ]
---   }
 
 
 view : Model -> Browser.Document Msg
@@ -246,12 +260,31 @@ paragraph_text paragraph =
     Just t -> t
     Nothing -> "<no text>"
 
-viewParagraph : Paragraph -> Element Msg
-viewParagraph paragraph =
+editItem : Maybe String -> Element Msg
+editItem paragraph =
+ Element.column [width fill]
+   [ row [width fill]
+      [ Input.button [Element.padding 10] {onPress = Just SaveEditItem, label = Element.text "Save"}
+      , Input.button [Element.padding 10] {onPress = Nothing, label = Element.text "Cancel"}
+      ]
+   , Input.multiline [ Border.rounded 4, width fill ] 
+      { onChange = TextChanged 
+      , text = Maybe.withDefault "" paragraph
+      , placeholder = Nothing 
+      , label = Input.labelHidden "default is fill?"
+      , spellcheck = True }
+  ]
+
+editParagraph : Maybe String -> Element Msg
+editParagraph paragraph =
+  editItem paragraph
+
+viewParagraph1 : Paragraph -> Element Msg
+viewParagraph1 paragraph =
   case paragraph.type_ of
-    "paragraph" -> paragraph |> paragraph_text |> WikiHelper.resolveLinks
+    "paragraph" -> paragraph |> resolveLinks
     "markdown"  -> 
-        (case MarkdownUi.view (paragraph |> paragraph_text |> WikiHelper.resolveLinks_markdown ) of
+        (case MarkdownUi.view (paragraph |> paragraph_text |> resolveLinks_markdown ) of
             Ok rendered ->
                 Element.column
                     [ Element.spacing 30
@@ -265,17 +298,24 @@ viewParagraph paragraph =
 
 
 
-    _ -> paragraph |> paragraph_text |> WikiHelper.resolveLinks
+    _ -> paragraph |> resolveLinks
+
+
+viewParagraph : Model -> Paragraph -> Element Msg
+viewParagraph model paragraph =
+  if paragraph.id == model.pselected
+  then editParagraph model.ptext
+  else viewParagraph1 paragraph
 
 storyButtonBar =
  row []
-    [ Input.button [] {onPress = Nothing, label = text "Save"}
-    , Input.button [] {onPress = Nothing, label = text "Cancel"}
+    [ Input.button [] {onPress = Nothing, label = Element.text "Save"}
+    , Input.button [] {onPress = Nothing, label = Element.text "Cancel"}
     ]
 
 
-viewStory : Story -> Element Msg
-viewStory story =
+viewStory : Model -> Story -> Element Msg
+viewStory model story =
   let 
     borderedColumn =
       column
@@ -295,13 +335,13 @@ viewStory story =
         borderedColumn 
         (storyButtonBar
         :: (Element.el [ centerX, padding 30, Font.bold, Font.size 24 ] (Element.text story.title))
-        :: (List.map viewParagraph story.story))
+        :: (List.map (viewParagraph model) story.story))
 
 
 viewStories : Model -> Element Msg
 viewStories model =
       Element.row [ spacing 10, padding 20 ]
-        (List.map viewStory (List.reverse model.stories))
+        (List.map (viewStory model) (List.reverse model.stories))
 
 -- HTTP
 
@@ -316,11 +356,12 @@ getRandomStory (Wiki.Slug slugname) =
     , expect = Http.expectJson GotStory storyDecoder
     }
 
+
 paragraphDecoder : Json.Decode.Decoder Paragraph
 paragraphDecoder =
   Json.Decode.map3 Paragraph
     (Json.Decode.field "type" Json.Decode.string)
-    (Json.Decode.field "id" Json.Decode.string )
+    (Json.Decode.maybe (Json.Decode.field "id" (Json.Decode.map ParaId Json.Decode.string)))
     (Json.Decode.maybe (Json.Decode.field "text" Json.Decode.string ))
 
 storyDecoder : Json.Decode.Decoder Story
@@ -331,3 +372,142 @@ storyDecoder =
 --    (Json.Decode.field "source" Json.Decode.string)
 --    (Json.Decode.field "author" Json.Decode.string)
 --    (Json.Decode.field "year" int)
+
+
+type Para =
+    PlainText String
+  | Link      String
+
+
+plain_words_helper : String -> Parser String
+plain_words_helper link_text =
+  if String.isEmpty link_text
+  then
+    Parser.problem "plain words cannot be 0 length"
+  else
+    Parser.succeed link_text
+
+plain_parse_to_next_link : Parser String
+plain_parse_to_next_link =
+  Parser.getChompedString (
+    Parser.chompWhile (\c -> c /= '[' && c /= '\n' && c /= '\r') )
+
+start_plain_parse : Parser ()
+start_plain_parse =
+  Parser.succeed ()
+  |. Parser.chompIf (\c -> c == '[' )
+
+plain_words : Parser String
+plain_words =
+  plain_parse_to_next_link
+  |> Parser.andThen plain_words_helper
+
+plain_words2 : Parser String
+plain_words2 =
+  Parser.getChompedString (
+    Parser.chompWhile (\c -> c /= '\n' && c /= '\r') )
+    |> Parser.andThen plain_words_helper
+
+link_words : Parser String
+link_words =
+  Parser.getChompedString <|
+    Parser.succeed ()
+      |. Parser.chompWhile (\c -> c /= ']' )
+
+internal_words : Parser Para
+internal_words =
+  Parser.succeed PlainText
+    |= plain_words
+
+internal_words2 : Parser Para
+internal_words2 =
+  Parser.succeed PlainText
+    |= plain_words2
+
+
+internal_link : Parser Para
+internal_link =
+  Parser.succeed Link
+    |. Parser.symbol "[["
+    |= link_words
+    |. Parser.symbol "]]"
+
+internal_link_or_word : Parser Para
+internal_link_or_word =
+  Parser.oneOf 
+    [ internal_link
+    , internal_words
+    , internal_words2
+    ]
+
+internal_link_or_words : Parser (List Para)
+internal_link_or_words =
+  Parser.loop [] internal_link_or_words_helper
+
+internal_link_or_words_helper : List Para -> Parser (Parser.Step (List Para) (List Para))
+internal_link_or_words_helper revParas =
+  Parser.oneOf
+    [ Parser.succeed (\para -> Parser.Loop (para :: revParas))
+        |= internal_link_or_word
+    , Parser.succeed ()
+        |> Parser.map (\_ -> Parser.Done (List.reverse revParas))
+    ]
+
+
+-- "http://fed.wiki/welcome-visitors.json
+
+fw_url : Wiki.Slug -> String
+fw_url (Wiki.Slug slugname) =
+--  "http://fed.wiki/" ++ slugname ++ ".json"
+  "/" ++ slugname
+
+
+textChanged : String -> Msg
+textChanged str =
+  TextChanged str
+
+para_to_html : Para -> Element Msg
+para_to_html p =
+  case p of
+    Link link_string -> 
+      let slug = Wiki.asSlug link_string in
+--        Html.a [ Html.Attributes.href (fw_url slug) ] [ Html.text link_string ]
+        Element.link [ Font.bold, Font.underline, Font.color (rgb255 0x72 0x9F 0xCF) ] { url = (fw_url slug), label = Element.text link_string }
+
+    PlainText text_string -> Element.text text_string
+-- { onChange : String -> msg, text : String, placeholder : Maybe (Input.Placeholder msg), label : Input.Label msg } -> Element msg
+--    PlainText text_string -> Input.text [ Border.rounded 4 ] { onChange = TextChanged , text = text_string , placeholder = Nothing , label = Input.labelAbove [] <| text "default is fill?" }
+
+para_to_markdown : Para -> String
+para_to_markdown p =
+  case p of
+    Link link_string -> 
+      let slug = Wiki.asSlug link_string in
+        "[" ++ link_string ++ "](" ++ (fw_url slug) ++ ")"
+
+    PlainText text_string -> text_string
+
+
+text_to_html : Maybe ParaId -> String -> Element Msg
+text_to_html para_id para_text =
+  case Parser.run internal_link_or_words para_text of
+    Ok para_ast -> Element.paragraph [padding 10, Events.onDoubleClick (SelectParagraph para_id) ] (List.map para_to_html para_ast)
+    Err _ -> Element.text "parse error: "
+
+
+text_to_markdown : String -> String
+text_to_markdown para_text =
+  case Parser.run internal_link_or_words para_text of
+    Ok para_ast -> (List.map para_to_markdown para_ast) |> String.join " "
+    Err _ -> "parse error: "
+
+
+resolveLinks : Paragraph -> Element Msg
+resolveLinks wiki_paragraph =
+    text_to_html wiki_paragraph.id (Maybe.withDefault "" wiki_paragraph.text)
+
+resolveLinks_markdown : String -> String
+resolveLinks_markdown wiki_text =
+    wiki_text
+    |> text_to_markdown
+
--- tests/Example.elm
+++ tests/Example.elm
@@ -8,7 +8,7 @@ import Test exposing (..)
 import Parser exposing (Parser, (|.), (|=), succeed, symbol, float, spaces)
 import Html exposing (text)
 
-import WikiHelper
+import Main
 import Wiki
 
 type alias Point =
@@ -40,17 +40,17 @@ link_words : Test
 link_words =
   describe "Test internal linking words with [[ ... ]] notations"
   [ test "Parse some plain words" <|
-      \_ -> Expect.equal ( Ok ("hello world") ) ( Parser.run WikiHelper.plain_words "hello world")
+      \_ -> Expect.equal ( Ok ("hello world") ) ( Parser.run Main.plain_words "hello world")
   , test "Parse some link words" <|
-    \_ -> Expect.equal (Ok ( WikiHelper.Link ("hello world") ) ) ( Parser.run WikiHelper.internal_link_or_word "[[hello world]]")
+    \_ -> Expect.equal (Ok ( Main.Link ("hello world") ) ) ( Parser.run Main.internal_link_or_word "[[hello world]]")
   , test "Parse some link words 2" <|
-    \_ -> Expect.equal (Ok ( WikiHelper.PlainText ("[hello world") ) ) ( Parser.run WikiHelper.internal_link_or_word "[hello world")
+    \_ -> Expect.equal (Ok ( Main.PlainText ("[hello world") ) ) ( Parser.run Main.internal_link_or_word "[hello world")
   , test "Parse some link words 3" <|
-    \_ -> Expect.equal (Ok ( [ WikiHelper.Link ("hello world"), WikiHelper.PlainText " ", WikiHelper.Link ("hello world") ] ) ) ( Parser.run WikiHelper.internal_link_or_words "[[hello world]] [[hello world]]")
+    \_ -> Expect.equal (Ok ( [ Main.Link ("hello world"), Main.PlainText " ", Main.Link ("hello world") ] ) ) ( Parser.run Main.internal_link_or_words "[[hello world]] [[hello world]]")
   , test "Parse some link words 4" <|
-    \_ -> Expect.equal (Ok ( [ WikiHelper.PlainText ("add more writing spaces. Read "), WikiHelper.Link "How to Wiki", WikiHelper.PlainText (" for more ideas. Follow "), WikiHelper.Link "Recent Changes", WikiHelper.PlainText " here and nearby." ] ) ) ( Parser.run WikiHelper.internal_link_or_words "add more writing spaces. Read [[How to Wiki]] for more ideas. Follow [[Recent Changes]] here and nearby.")
+    \_ -> Expect.equal (Ok ( [ Main.PlainText ("add more writing spaces. Read "), Main.Link "How to Wiki", Main.PlainText (" for more ideas. Follow "), Main.Link "Recent Changes", Main.PlainText " here and nearby." ] ) ) ( Parser.run Main.internal_link_or_words "add more writing spaces. Read [[How to Wiki]] for more ideas. Follow [[Recent Changes]] here and nearby.")
   , test "Parse some link words 5" <|
-    \_ -> Expect.equal (Ok ( [ WikiHelper.PlainText "You can edit your copy of these pages.  Press ", WikiHelper.PlainText "[+] to add more writing spaces. Read ", WikiHelper.Link "How to Wiki", WikiHelper.PlainText (" for more ideas. Follow "), WikiHelper.Link "Recent Changes", WikiHelper.PlainText " here and nearby." ] ) ) ( Parser.run WikiHelper.internal_link_or_words "You can edit your copy of these pages. Press [+] to add more writing spaces. Read [[How to Wiki]] for more ideas. Follow [[Recent Changes]] here and nearby.")
+    \_ -> Expect.equal (Ok ( [ Main.PlainText "You can edit your copy of these pages.  Press ", Main.PlainText "[+] to add more writing spaces. Read ", Main.Link "How to Wiki", Main.PlainText (" for more ideas. Follow "), Main.Link "Recent Changes", Main.PlainText " here and nearby." ] ) ) ( Parser.run Main.internal_link_or_words "You can edit your copy of these pages. Press [+] to add more writing spaces. Read [[How to Wiki]] for more ideas. Follow [[Recent Changes]] here and nearby.")
   ]
 
 
